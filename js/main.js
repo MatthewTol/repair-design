5dbe1a5816bbbd85522dca8b1f46e799a310e686
Matthew <matveytol123@gmail.com>
2019-12-19 23:30:47 +0300

    feat: switch fucntion for modal window

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  }
+
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal)
+  });
+
+  closeBtn.addEventListener('click', switchModal);
+
+});
