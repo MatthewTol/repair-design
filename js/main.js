2e14d0ffa3771418ac3667f505432f5c773a73b8
Matthew <matveytol123@gmail.com>
2019-12-20 20:33:21 +0300

    refact: rewrote my javascript code in jquery code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
+// document.addEventListener("DOMContentLoaded", function(event) { 
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBtn = document.querySelector('.modal__close');
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible');
+//   }
 
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal)
+//   modalBtn.forEach(element => {
+//     element.addEventListener('click', switchModal)
+//   });
+
+//   closeBtn.addEventListener('click', switchModal);
+
+// });
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
   });
-
-  closeBtn.addEventListener('click', switchModal);
-
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
 });
