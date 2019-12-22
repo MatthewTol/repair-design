844ae4fe081160eea204582522e5615a139a4e6b
Matthew <matveytol123@gmail.com>
2019-12-22 11:48:41 +0300

    feat: made projects section and added sliders

--- js/main.js
+++ js/main.js
@@ -25,4 +25,22 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  bullets.css('left', prev.width() + 10);
+  next.css('left', prev.width() + 10 + bullets.width() + 10);
 });
